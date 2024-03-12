100324cc4a86c45a7090d880f930d8ac6f86d1d8
Jason A. Ballard <106720990+JBtallgrass@users.noreply.github.com>
2024-03-11 23:51:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,95 @@
+import plotly.express as px
+from shiny.express import input, ui
+from shiny import render
+from shinywidgets import render_plotly
+import pandas as pd
+import seaborn as sns
+import palmerpenguins  # This package provides the Palmer Penguins dataset
 
+# built-in function to load the Palmer Penguins dataset
+penguins_df = palmerpenguins.load_penguins()
+
+# Page name
+ui.page_opts(title="JB Penguins Data", fillable=True)
+
+# sidebar for user interaction
+with ui.sidebar(open="open"):
+    ui.h2("Sidebar")
+    ui.input_selectize(
+        "selected_attribute",
+        "Select Plotly Attribute",
+        ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
+    )
+
+    # Create a numeric input for the number of Plotly histogram bins
+    ui.input_numeric("plotly_bin_count", "Number of plotly bins", 30)
+
+    # Creates slider input for Seaborn bins
+    ui.input_slider(
+        "seaborn_bin_slider",
+        "Number of Bins",
+        1,
+        50,
+        10,
+    )
+
+    # Use ui.input_checkbox_group() to create a checkbox group input to filter the species
+    ui.input_checkbox_group(
+        "selected_species_list",
+        "Species in Scatterplot",
+        ["Adelie", "Gentoo", "Chinstrap"],
+        selected=["Adelie"],
+        inline=True,
+    )
+
+# Use ui.hr() to add a horizontal rule to the sidebar
+ui.hr()
+
+# Use ui.a() to add a hyperlink to the sidebar
+ui.a(
+    "JBTallgrass GitHub",
+    href="https://github.com/JBtallgrass/cintel-02-data",
+    target="_blank",
+)
+
+# Data table showing the penguin dataset Include 2 cards with a table and a grid
+with ui.layout_columns(col_widths=(4, 8)):
+    with ui.card(full_screen=True):  # Full screen option
+        ui.h3("Penguins Data Table")
+
+        @render.data_frame
+        def render_penguins_table():
+            return penguins_df
+
+    with ui.card(full_screen=True):
+        ui.h3("Penguins Data Grid")
+
+        @render.data_frame
+        def render_penguins_grid():
+            return penguins_df
+
+
+# Use ui.hr() to add a horizontal rule to the sidebar
+ui.hr()
+
+# Creates a Plotly Histogram showing all species
+with ui.layout_columns():
+    with ui.card(full_screen=True):
+        ui.h3("All Species Histogram-Plotly")
+
+    @render_plotly
+    def plotly_histogram():
+        return px.histogram(penguins_df, x="species")
+
+    with ui.card(full_screen=True):
+        ui.h3("All Species ScatterPlot-plotly")
+
+    @render_plotly
+    def plotly_scatterplot():
+        return px.scatter(
+            penguins_df,
+            title="All Species ScatterPlot-plotly",
+            x="body_mass_g",
+            y="bill_length_mm",
+            color="species",
+            symbol="species",
